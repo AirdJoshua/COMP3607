0aff1efce51fe4499a5945604d7b1a82d9e17278
Kemeisha_W <84978204+Kemeisha-W@users.noreply.github.com>
2021-11-16 19:55:32 -0400

    Tests completed

--- groupassignment/src/test/java/com/groupassignment/OriginalNameChangeTest.java
+++ groupassignment/src/test/java/com/groupassignment/OriginalNameChangeTest.java
@@ -15,7 +15,9 @@ import org.junit.Before;
 
 public class OriginalNameChangeTest {
     private IChangeName newName;
-    List<Student> students = new ArrayList();
+    List<Student> students = new ArrayList<Student>();
+    Student student = new Student("Participant 601725", "Maryann Steele", "81304376", "maryann.steele@my.uwi.edu", "", "", "100.00", "Yes", "-", "");
+        
 
     public OriginalNameChangeTest(){
 
@@ -31,10 +33,9 @@ public class OriginalNameChangeTest {
     
     @Before
     public void setUp() {
-        Student student = new Student("Participant 601725", "Maryann Steele", "81304376", "maryann.steele@my.uwi.edu", "", "", "100.00", "Yes", "-", "");
         students.add(student); 
         File file = new File("C:\\Users\\willk\\Documents\\GitHub\\COMP3607\\1420037856-602106_Maryann_Steele_601725_Info 2603 Assignment 1.pdf");
-        newName = new OriginalNameChange(student, file, students);
+        newName = new OriginalNameChangeStrategy(file, students);
     }
     
     @After
@@ -42,7 +43,7 @@ public class OriginalNameChangeTest {
     }
 
     @Test
-    public void testChangePdf(){
+    public void testChangePdfName(){
         System.out.println("get New name");
         String expResults = "Maryann Steele_601725_assignsubmission_file_Info 2603 Assignment 1.pdf";
         String results = newName.changePdfName();
@@ -50,15 +51,19 @@ public class OriginalNameChangeTest {
     }
 
     @Test
-    public Student testFindStudent(){
+    public void testFindStudent(){
         System.out.println("Find Student");
-        Student expResults = ;
+        Student expResults = student;
         Student results = newName.findStudent("Maryann Steele");
-
-
+        assertEquals(expResults, results);
     }
 
     @Test
-
+    public void testFindById(){
+        System.out.println("Find by ID");
+        Student expResults = student;
+        Student results = ((OriginalNameChangeStrategy) newName).findById("Maryann Steele");
+        assertEquals(expResults, results);
+    }
     
 }
