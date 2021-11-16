369ebf1a1a0fb64c51f352c036846078086e4eb1
Kemeisha_W <84978204+Kemeisha-W@users.noreply.github.com>
2021-11-16 14:49:07 -0400

    commit

---
+++ groupassignment/src/test/java/com/groupassignment/OriginalNameTest.java
@@ -0,0 +1,64 @@
+package com.groupassignment;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import org.junit.BeforeClass;
+import org.junit.Before;
+
+public class OriginalNameTest {
+    private IChangeName newName;
+    List<Student> students = new ArrayList();
+
+    public OriginalNameTest(){
+
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        Student student = new Student("Participant 601725", "Maryann Steele", "81304376", "maryann.steele@my.uwi.edu", "", "", "100.00", "Yes", "-", "");
+        students.add(student); 
+        File file = new File("C:\\Users\\willk\\Documents\\GitHub\\COMP3607\\1420037856-602106_Maryann_Steele_601725_Info 2603 Assignment 1.pdf");
+        newName = new OriginalNameChange(student, file, students);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testChangePdf(){
+        System.out.println("get New name");
+        String expResults = "Maryann Steele_601725_assignsubmission_file_Info 2603 Assignment 1.pdf";
+        String results = newName.changePdfName();
+        assertEquals(expResults,results);
+    }
+
+    @Test
+    public Student testFindStudent(){
+        System.out.println("Find Student");
+        Student expResults = ;
+        Student results = newName.findStudent("Maryann Steele");
+
+
+    }
+
+    @Test
+
+    
+}
